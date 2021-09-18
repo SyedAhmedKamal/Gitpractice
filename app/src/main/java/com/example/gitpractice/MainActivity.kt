fb026b25f8c5db75abddc8cc50da44a7af56a0b8
ImSyedAhmed <kamalahmed853@gmail.com>
2021-09-18 22:09:24 +0500

    Revert "This is a third commit."
    This reverts commit fbe19b40568558a796e99870762ecb98070fe332.

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("this is second dummy commit")
-        jgj kbkm ,h
+        mmn,jll;k;kj;
     }
 }
