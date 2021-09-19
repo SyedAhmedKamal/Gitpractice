2377ea35cede394ca660d7bac2067be54e6568d5
ImSyedAhmed <kamalahmed853@gmail.com>
2021-09-19 10:42:49 +0500

    New commit, commit four. Last

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("this is second dummy commit")
+        println("Last commit so far.")
     }
 }
